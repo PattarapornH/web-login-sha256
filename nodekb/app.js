0cb57d40869be22279b46355144ee5275f55f7f3
PattarapornH <pataraporn.han@gmail.com>
2018-09-09 01:18:24 +0700

    edit page can't update

--- nodekb/app.js
+++ nodekb/app.js
@@ -80,7 +80,35 @@ app.post('/articles/add',function(req,res) {
             res.redirect('/');
         }
     });
+});
 
+//load edit form
+app.get('/article/edit/:id', function (req, res) {
+    Article.findById(req.params.id, function (err, article) {
+        res.render('edit_article', {
+            title:'Edit Article',
+            article: article
+        });
+    });
+});
+
+//update submit POST route
+app.post('/articles/edit/:id', function (req, res) {
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id:req.params.id}
+
+    Article.update(query,article,function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
 //start server
