9c909d10621dd8fdcabd9d0c1dfc6145c236bd6f
PattarapornH <pataraporn.han@gmail.com>
2018-09-08 20:13:18 +0700

    part6 dropdown not work

--- nodekb/app.js
+++ nodekb/app.js
@@ -33,6 +33,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+//set public folder
+app.use(express.static(path.join(__dirname,'public')));
+
 //home route
 app.get('/', function(req,res){
     Article.find({},function(err,articles) {
@@ -44,6 +47,14 @@ app.get('/', function(req,res){
                 articles : articles
             });
         }
+    });
+});
+
+//get a single article
+app.get('/article/:id',function(req,res){
+    Article.findById(req.params.id, function(err, article){
+        console.log(article);
+        return;
     });
 });
 
