79b00b671c446fbe8635b68a1343babbdb5ccc1a
PattarapH <pataraporn.han@gmail.com>
2018-09-08 15:03:52 +0700

    init commit

---
+++ nodekb/app.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb:localhost/nodekb');
+let db = mongoose.connection;
+
+//check connection
+db.once('open', function(){
+    console.log('Connected to MongoDB');
+});
+
+//check for db error
+db.on('error',function(err){
+    console.log(err);
+});
+
+//init app
+const app = express();
+
+//bring in models
+let Article = require('./models/article');
+
+// app.engine('pug', require('pug').__express)
+//load view engine
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
+
+
+//home route
+app.get('/', function(req,res){
+    Article.find({}, function(err,articles){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render('index', {
+                title : 'Articles',
+                articles: articles
+            });
+        }
+    });
+    // let articles = [
+    //     {
+    //         id : 5910502026,
+    //         title : "Article One",
+    //         author : 'Pattarporn Hanphadungkit',
+    //         body : 'This is article one'
+    //     },
+    //     {
+    //         id : 2,
+    //         title : "Article Two",
+    //         author : 'Pattarporn Hanphadungkit',
+    //         body : 'This is article two'
+    //     },
+    //     {
+    //         id : 3,
+    //         title : "Article One",
+    //         author : 'Pattarporn Hanphadungkit',
+    //         body : 'This is article three'
+    //     },
+    // ];
+    
+    // res.send('Hello World');
+});
+
+// Add Route
+app.get('/articles/add', function(req,res){
+    res.render('add_article',{
+        title : 'Add Article'
+    });
+});
+
+//start server
+app.listen(3000,function(){
+    console.log('Server started on port 3000...')
+});
